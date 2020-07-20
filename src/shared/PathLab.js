f195e594ded640da4bce429058304d4d2910f02f
vind9961 <64213773+vind9961@users.noreply.github.com>
2020-07-21 00:31:02 +0530

    Update PathLab.js

--- src/shared/PathLab.js
+++ src/shared/PathLab.js
@@ -4,20 +4,20 @@ export const Lab = [
     detail: "Include 47 parmeter",
     price: "$80",
     off: "70%",
-    img: '/assests/img/family1.jpg'
+    img: 'public/assests/img/family1.jpg'
   },
   {
     name: "Comprehensive Full Body Checkup",
     detail: "Include 47 parmeter",
     price: "$90",
     off: "67%",
-    img: '/assests/img/family2.jpg'
+    img: 'public/assests/img/family2.jpg'
   },
   {
     name: "Advance Full Body Checkup",
     detail: "Include 100 parmeter",
     price: "$100",
     off: "70%",
-    img: '/assests/img/family3.jpg'
+    img: 'public/assests/img/family3.jpg'
   }
 ];
